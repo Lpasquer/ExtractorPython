44006b980e6a1a3849ec547e3dd8dbc67b8da173
FlavLdc <57667777+FlavLdc@users.noreply.github.com>
2020-12-03 13:59:03 +0100

    Update script

--- Script.cmd
+++ Script.cmd
@@ -1,13 +1,19 @@
+call echo "Se mettre dans le répertoire du projet Java"
 cd C:\Users\Admin\Documents\GitHub\PDLProject
 
+call echo "Installation"
 mvn install
 
+call echo "Compilation"
 mvn compile
 
+call echo "Execution de l'extracteur Java"
 call java -cp "target/WikipediaMatrix-1.0-SNAPSHOT.jar" pdl/wiki/WikipediaMatrix
 
+call echo "Se mettre dans le répertoire du projet python"
 cd C:\Users\Admin\Documents\GitHub\ExtractorPython
 
+call echo "Ouverture de l'extracteur python"
 call extracteur.py
 
 
